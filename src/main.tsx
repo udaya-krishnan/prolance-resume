0395038c5b5f31876058f6cab6ffb587be7ca336
udaya-krishnan <udayankrishnan36@gmail.com>
2026-07-27 15:15:00 +0530

    add the scrolltotop

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
      <ScrollToTop />
-      <App />
+      <App />    
     </BrowserRouter>
   </React.StrictMode>
 )
